08a8d99bea2e3abf53f7cdcce23454a6dd5d5924
ranzdhika <94164759+RANZEIN@users.noreply.github.com>
2026-04-22 19:23:05 +0700

    Refactor layout component to use a unified Providers component, removing direct imports of PrimeReactProvider and LayoutProvider for cleaner structure.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,4 @@
-'use client';
-import { LayoutProvider } from '../layout/context/layoutcontext';
-import { PrimeReactProvider } from 'primereact/api';
-import 'primereact/resources/primereact.css';
-import 'primeflex/primeflex.css';
-import 'primeicons/primeicons.css';
-import '../styles/layout/layout.scss';
-import '../styles/demo/Demos.scss';
+import Providers from './providers';
 
 interface RootLayoutProps {
     children: React.ReactNode;
@@ -18,9 +11,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 <link id="theme-css" href={`/themes/lara-light-indigo/theme.css`} rel="stylesheet"></link>
             </head>
             <body>
-                <PrimeReactProvider>
-                    <LayoutProvider>{children}</LayoutProvider>
-                </PrimeReactProvider>
+                <Providers>{children}</Providers>
             </body>
         </html>
     );
