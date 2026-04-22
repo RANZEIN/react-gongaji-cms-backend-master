1b1132c085c2e644b12d257de4847c76e372c8d0
ranzdhika <94164759+RANZEIN@users.noreply.github.com>
2026-04-22 19:42:51 +0700

    Add constants module for configuration values including base URLs and authentication settings

---
+++ utils/constants.ts
@@ -0,0 +1,6 @@
+export const BASE_URL_AUTH = process.env.NEXT_PUBLIC_BASE_URL_AUTH ?? 'https://gongaji-authentication-service-m3gra3glsq-et.a.run.app';
+export const BASE_URL_ARTICLE = process.env.NEXT_PUBLIC_BASE_URL_ARTICLE ?? 'https://gongaji-article-service-m3gra3glsq-et.a.run.app';
+export const STATIC_BEARER_TOKEN = process.env.NEXT_PUBLIC_STATIC_BEARER_TOKEN ?? '';
+export const AUTH_CLIENT_ID = process.env.NEXT_PUBLIC_AUTH_CLIENT_ID ?? 'cms-web';
+
+export const AUTH_COOKIE_NAME = 'authToken';
