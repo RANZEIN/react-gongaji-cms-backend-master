a17bc4e7b9a75796fe059718202586586a9177b7
Ranzein <ranzein@gmail.com>
2026-04-29 16:18:18 +0700

    Add BASE_URL_STORE constant to manage store service URL configuration. This allows for dynamic URL assignment based on environment variables, enhancing flexibility in service integration.

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,6 @@
 export const BASE_URL_AUTH = process.env.NEXT_PUBLIC_BASE_URL_AUTH ?? 'https://gongaji-authentication-service-m3gra3glsq-et.a.run.app';
 export const BASE_URL_ARTICLE = process.env.NEXT_PUBLIC_BASE_URL_ARTICLE ?? 'https://gongaji-article-service-m3gra3glsq-et.a.run.app';
+export const BASE_URL_STORE = process.env.NEXT_PUBLIC_BASE_URL_STORE ?? '';
 export const STATIC_BEARER_TOKEN = process.env.NEXT_PUBLIC_STATIC_BEARER_TOKEN ?? '';
 export const AUTH_CLIENT_ID = process.env.NEXT_PUBLIC_AUTH_CLIENT_ID ?? 'cms-web';
 
